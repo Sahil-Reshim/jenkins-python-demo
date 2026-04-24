e56e9b2501f2a0cde67606e9f45f1f94a2ae1331
Sahil-Reshim <sahilreshim441@gmail.com>
2026-04-24 13:33:33 +0530

    Final Deployment

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask app deployed automatically by Jenkins!"
+    return "Flask app deployed automatically by Jenkins! Webhook triggered the build and deployment process."
